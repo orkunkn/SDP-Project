d08af4e1b8e44421f429fb06c3fca882840fecf4
Orkun Kayman <orkunkaymann@gmail.com>
2024-03-22 18:11:56 +0300

    - Added logs - Added time measurement - Optimized the code (works about %15 faster) - Fixed bugs

--- constructor.py
+++ constructor.py
@@ -9,8 +9,10 @@ class Constructor:
         self.env.total_nodes = self.env.G.number_of_nodes()
         self.env.indegree_dict = {node: self.env.G.in_degree(node) for node in self.env.G.nodes()}
         
-
         self.env.level_costs = {}
+        self.env.node_count_per_level = {}
+        max_level = 0
+        
         for node, level in self.env.levels.items():
             indegree = self.env.indegree_dict[node]
             cost = max(0, 2 * indegree - 1)
@@ -20,44 +22,24 @@ class Constructor:
             else:
                 self.env.level_costs[level] = cost
 
-
-        # Count nodes in every level
-        self.env.node_count_per_level = {}
-        for node, level in self.env.levels.items():
+            # Count nodes in every level
             if level in self.env.node_count_per_level:
                 self.env.node_count_per_level[level] += 1
             else:
                 self.env.node_count_per_level[level] = 1
-         
+            
+            if max_level < level:
+                max_level = level
+
         total_parents = sum(self.env.indegree_dict.values())
 
         # 3 criterias to control
         # Average Indegree
         self.env.AIR = total_parents / self.env.total_nodes if self.env.total_nodes else 0
         # Average Row per Level
-        self.env.ARL = self.env.total_nodes / (max(self.env.levels.values()) + 1) if self.env.levels else 0
+        self.env.ARL = self.env.total_nodes / max_level if self.env.levels else 0
         # Average Level Cost
-        self.env.ALC = self.calculate_alc()
-
-
-        # Check each node's calculated value against ALC and store their level and node with indegree
-        for node in self.env.G.nodes():
-            value = 2 * self.env.indegree_dict[node] - 1
-            if value < self.env.ALC:
-                node_level = self.env.levels[node]
-                # Add node and its indegree to the respective level
-                if node_level not in self.env.state_level_vectors:
-                    self.env.state_level_vectors[node_level] = {}
-                self.env.state_level_vectors[node_level][node] = self.env.indegree_dict[node]
-
-        # Check each node's calculated value against ALC and store their level and node with indegree
-        for node in self.env.G.nodes():
-            if value < self.env.ALC:
-                node_level = self.env.levels[node]
-                # Add node and its indegree to the respective level
-                if node_level not in self.env.state_level_vectors:
-                    self.env.state_level_vectors[node_level] = {}
-                self.env.state_level_vectors[node_level][node] = self.env.indegree_dict[node]
+        self.env.ALC = sum(self.env.level_costs.values()) / (max_level + 1) if max_level + 1 > 0 else 0
 
         self.env.nodes_in_thin_levels_mapping = self.find_nodes_in_thin_levels()
         self.update_levels_of_nodes_in_thin()
@@ -79,25 +61,6 @@ class Constructor:
             grandparents.update(self.env.G.predecessors(parent))
 
         return len(grandparents)
-
-
-    def get_by_thin_levels(self):
-        for levels in self.env.state_level_vectors:
-            sub_dict = self.env.state_level_vectors[levels]
-
-            # Calculate the frequency of each value
-            value_frequencies = {}
-            for value in sub_dict.values():
-                if value in value_frequencies:
-                    value_frequencies[value] += 1
-                else:
-                    value_frequencies[value] = 1
-
-            # Sorting each inner dictionary by the frequency of its values (from lowest to highest)
-            sorted_sub_dict = dict(sorted(sub_dict.items(), key=lambda item: value_frequencies[item[1]]))
-            self.env.state_level_vectors[levels] = sorted_sub_dict
-
-        return self.env.state_level_vectors
     
     # Finds the nodes in thin levels and returns a mapping to their actual node number, starting from 0
     def find_nodes_in_thin_levels(self):
@@ -129,12 +92,7 @@ class Constructor:
         for node, level in self.env.levels_of_nodes_in_thin.items():
             self.env.levels_of_nodes_in_thin[node] = self.env.levels[node]
             self.env.indegrees_of_nodes_in_thin[node] = self.env.indegree_dict[node]
-        
-    def calculate_alc(self):
-        """ Recalculate the Average Level Cost (ALC) after a node is moved. """
-        ALC_numerator = sum(max(2 * indegree - 1, 0) for indegree in self.env.indegree_dict.values())
-        return ALC_numerator / (max(self.env.levels.values()) + 1) if self.env.levels.values() else 0
-
+    
 
     def generate_info_text(self):
         """ Generate information text about the graph metrics. """
@@ -143,5 +101,5 @@ class Constructor:
             f"Total Levels: {max(self.env.levels.values()) + 1}\n"
             f"Indegree of Each Node: {self.env.indegree_dict}\n"
             f"Average Indegree per Row (AIR): {self.env.AIR:.2f}\n"
-            f"Average Level Cost (ALC): {self.calculate_alc():.2f}\n"
+            f"Average Level Cost (ALC): {self.env.ALC:.2f}\n"
             f"Average Number of Rows per Level (ARL): {self.env.total_nodes / (max(self.env.levels.values()) + 1):.2f}" )
